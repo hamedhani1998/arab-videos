254edc4065dea450122e3931a762ad0a5712ab8c
yemen <hanihameda@gmail.com>
2026-07-15 20:09:19 +0300

    Fix username in SexAlArab build.gradle.kts

--- SexAlArab/build.gradle.kts
+++ SexAlArab/build.gradle.kts
@@ -2,9 +2,9 @@ version = 1
 
 cloudstream {
     description = "إضافة سكس العرب - أفلام إباحية عربية مترجمة"
-    authors = listOf("hanihamed1998")
+    authors = listOf("hamedhani1998")
     status = 1
     tvTypes = listOf("NSFW")
     language = "ar"
-    iconUrl = "https://raw.githubusercontent.com/hanihamed1998/my-extensions-repo/main/icons/SexAlArab.png"
+    iconUrl = "https://raw.githubusercontent.com/hamedhani1998/my-extensions-repo/main/icons/SexAlArab.png"
 }
